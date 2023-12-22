c78744db8678f6f7f7b9ca52602a158eee842602
StarHeartHunt <starheart233@gmail.com>
2023-12-22 22:24:58 +0800

    feat: generic swagger response

--- src/positions/positions.controller.ts
+++ src/positions/positions.controller.ts
@@ -8,13 +8,13 @@ import {
   Post,
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { withBaseResponse } from '../interceptors/transform.interceptor';
 import { CreatePositionDto } from './dto/create-position.dto';
 import { UpdatePositionDto } from './dto/update-position.dto';
 import { PositionEntity } from './entities/position.entity';
 import { PositionsService } from './positions.service';
-// import { withBaseResponse } from 'src/interceptors/transform.interceptor';
 
-// class ResponsePositionDto extends withBaseResponse(PositionEntity, {}) {}
+class ResponsePositionDto extends withBaseResponse(PositionEntity, {}) {}
 
 @Controller('positions')
 @ApiTags('positions')
@@ -22,25 +22,25 @@ export class PositionsController {
   constructor(private readonly positionsService: PositionsService) {}
 
   @Post()
-  @ApiCreatedResponse({ type: PositionEntity })
+  @ApiCreatedResponse({ type: ResponsePositionDto })
   create(@Body() createPositionDto: CreatePositionDto) {
     return this.positionsService.create(createPositionDto);
   }
 
   @Get()
-  @ApiOkResponse({ type: PositionEntity, isArray: true })
+  @ApiOkResponse({ type: ResponsePositionDto, isArray: true })
   findAll() {
     return this.positionsService.findAll();
   }
 
   @Get(':id')
-  @ApiOkResponse({ type: PositionEntity })
+  @ApiOkResponse({ type: ResponsePositionDto })
   findOne(@Param('id') id: string) {
     return this.positionsService.findOne(+id);
   }
 
   @Patch(':id')
-  @ApiOkResponse({ type: PositionEntity })
+  @ApiOkResponse({ type: ResponsePositionDto })
   update(
     @Param('id') id: string,
     @Body() updatePositionDto: UpdatePositionDto,
@@ -49,7 +49,7 @@ export class PositionsController {
   }
 
   @Delete(':id')
-  @ApiOkResponse({ type: PositionEntity })
+  @ApiOkResponse({ type: ResponsePositionDto })
   remove(@Param('id') id: string) {
     return this.positionsService.remove(+id);
   }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,10 +8,13 @@ import {
   Post,
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { withBaseResponse } from '../interceptors/transform.interceptor';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductEntity } from './entities/product.entity';
 import { ProductsService } from './products.service';
+
+class ResponseProductDto extends withBaseResponse(ProductEntity, {}) {}
 
 @Controller('products')
 @ApiTags('products')
@@ -19,31 +22,31 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  @ApiCreatedResponse({ type: ProductEntity })
+  @ApiCreatedResponse({ type: ResponseProductDto })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
-  @ApiOkResponse({ type: ProductEntity, isArray: true })
+  @ApiOkResponse({ type: ResponseProductDto, isArray: true })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
-  @ApiOkResponse({ type: ProductEntity })
+  @ApiOkResponse({ type: ResponseProductDto })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
-  @ApiOkResponse({ type: ProductEntity })
+  @ApiOkResponse({ type: ResponseProductDto })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  @ApiOkResponse({ type: ProductEntity })
+  @ApiOkResponse({ type: ResponseProductDto })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
