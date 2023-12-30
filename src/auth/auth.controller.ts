f37adf7ab630bae711d8518f335d80e7c3e88b28
StarHeartHunt <starheart233@gmail.com>
2023-12-30 08:51:57 +0800

    feat: update api tag of auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { Public } from './utils';
 
 @Controller('auth')
+@ApiTags('auth')
 @Public()
 export class AuthController {
   constructor(private authService: AuthService) {}
