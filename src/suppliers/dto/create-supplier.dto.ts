dea93518b73fc359108f87851cd98ff6d9396145
StarHeartHunt <starheart233@gmail.com>
2023-12-30 22:21:11 +0800

    fix: fix validation

--- src/suppliers/dto/create-supplier.dto.ts
+++ src/suppliers/dto/create-supplier.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateSupplierDto {
   @ApiProperty()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   address: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   contact: string;
 }
